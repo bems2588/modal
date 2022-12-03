e4ac5a2077ffa467ccbb2d16cf561f8dc8cb45d1
Bruno Marcenaro <bems2588@gmail.com>
2022-12-03 07:29:14 -0500

    add classes

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) 
+for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', function () {
     console.log('Button clicked');
     modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+})
 
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+})
