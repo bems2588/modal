2283a9a43099364432e070e7ce88ed22a64a58b2
Bruno Marcenaro <bems2588@gmail.com>
2022-12-06 08:12:06 -0500

    add event

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-const openModal = function(){
+const openModal = function () {
   modal.classList.remove('hidden');
   over1.classList.remove('hidden');
 };
@@ -15,9 +15,12 @@ const closeModal = function () {
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click',openModal);
+  btnsOpenModal[i].addEventListener('click', openModal);
 
 
 btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-overlay.addEventListener('click', closeModal)
+document.addEventListener('keydown', function (event) {
+  console.log("A Key was pressed");
+})
