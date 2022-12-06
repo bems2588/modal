448189ee3a0cf62add5f1a84fff2df535a2c0c2c
Bruno Marcenaro <bems2588@gmail.com>
2022-12-05 21:52:41 -0500

    DRY code

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ for (let i = 0; i < btnsOpenModal.length; i++)
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
   });
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-})
 
-overlay.addEventListener('click', function () {
+
+const closeModel = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-})
+}
+btnCloseModal.addEventListener('click', closeModel);
+
+overlay.addEventListener('click', closeModel)
