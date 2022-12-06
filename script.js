dc75c2e436626e7d05c3062fa5fadc5fe9f4d46d
Bruno Marcenaro <bems2588@gmail.com>
2022-12-05 22:00:03 -0500

    even DRYer XD

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function(){
+  modal.classList.remove('hidden');
+  over1.classList.remove('hidden');
+};
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-
-
-const closeModel = function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-}
-btnCloseModal.addEventListener('click', closeModel);
+};
 
-overlay.addEventListener('click', closeModel)
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click',openModal);
+
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal)
